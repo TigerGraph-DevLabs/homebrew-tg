33b77998b1549692173aae02878ec3288088819f
Zrouga Mohamed <61499067+zrougamed@users.noreply.github.com>
2021-07-04 23:30:14 +0100

    Update tgcli.rb
    passing to version 0.3.0

--- tgcli.rb
+++ tgcli.rb
@@ -1,8 +1,8 @@
 class Tgcli < Formula
   desc "TigerGraphCLI: TigerGraph   "
   homepage "https://www.tigergraph.com"
-  url "https://github.com/TigerGraph-DevLabs/tgcli/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "d5cb094d2396329320b09cd7452ce3b5d4ea74e4691940fe942aa13175fa72e5"
+  url "https://github.com/TigerGraph-DevLabs/tgcli/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "3f0cb51294163e5c145caa6b0dc1f6f5ce7a10bc5a72f9cf86ce92b7093f75fe"
   license ""
 
   def install
