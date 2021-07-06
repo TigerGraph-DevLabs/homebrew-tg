e9047d0415d00bbd5581ae18ad4ebe642b3419a1
Zrouga Mohamed <61499067+zrougamed@users.noreply.github.com>
2021-07-06 14:41:07 +0100

    Update tgcli.rb

--- tgcli.rb
+++ tgcli.rb
@@ -1,8 +1,8 @@
 class Tgcli < Formula
   desc "TigerGraphCLI: TigerGraph   "
   homepage "https://www.tigergraph.com"
-  url "https://github.com/TigerGraph-DevLabs/tgcli/archive/refs/tags/0.4.0.tar.gz"
-  sha256 "a33efc374cef4b78d64ad679d0fbb9ed10a40483fd775016677283bd5c972fe4"
+  url "https://github.com/TigerGraph-DevLabs/tgcli/archive/refs/tags/0.5.0.tar.gz"
+  sha256 "9700d70486bc717aacd921ee28d7be0ce5aa6a2310a134f316c734a795ac441a"
   license ""
 
   def install
